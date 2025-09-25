56215d61eeab6973241c9d1afece742b4031e1e4
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-09-25 14:52:50 -0500

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -5,18 +5,11 @@ import base64
 import pandas as pd
 
 def standardize_numeric_column(series):
-    """
-    Convierte una serie de pandas a tipo numérico de forma robusta,
-    manejando comas como separadores decimales y otros errores.
-    """
     if pd.api.types.is_numeric_dtype(series):
         return series
-    # Reemplazar comas por puntos y convertir a numérico, forzando errores a NaN
     return pd.to_numeric(series.astype(str).str.replace(',', '.', regex=False), errors='coerce')
 
-
 def add_folium_download_button(folium_map, filename="mapa.html"):
-    """Añade un botón de descarga para un mapa de Folium."""
     map_html = folium_map.get_root().render()
     st.download_button(
         label=f"📥 Descargar Mapa ({filename})",
