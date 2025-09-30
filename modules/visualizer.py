c62c10be3b62aed1760a2abd95836c2099e92d70
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-09-30 17:44:48 -0500

    Update visualizer.py

--- modules/visualizer.py
+++ modules/visualizer.py
@@ -758,7 +758,7 @@ def display_advanced_maps_tab(gdf_filtered, stations_for_analysis, df_anual_melt
                     st.image(gif_bytes,
                              caption="Animación PPAM", 
                              width=600, 
-                             key=f'gif_viewer_{st.session_state.gif_reload_key}')
+                             
                 except Exception as e:
                     st.error(f"Ocurrió un error al intentar mostrar el GIF: {e}")
             else:
